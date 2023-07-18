07a109763e9565b507fa99893a4ceccca83a0d63
Carlos Perales <carlosperales050@gmail.com>
2023-07-17 20:39:21 -0600

    se agrega lista y pintar filas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './App.css'
+import { UserList } from './components/UserList/UserList';
+
+const URL = 'https://randomuser.me/api/?results=25'
+
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [color, setColor] = React.useState(false);
+
+  const toggleColor = () => {
+    setColor(!color);
+  }
+
+  React.useEffect(() => {
+    fetch(URL)
+      .then((resp) => resp.json())
+      .then((data) => setUsers(data.results))
+  }, [])
 
   return (
     <>
+      <h1> Random User list</h1>
+      <header>
+          <button onClick={toggleColor} >
+            change color rows
+          </button>
+      </header>
+      <main>
+        <UserList users={users}  color={color} />
+
+      </main>
+
+
     </>
   )
 }
---
+++ src/components/UserList/UserList.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import './userList.css'
+
+function UserList({ users ,color}) {
+
+    return (
+
+        <table className="tableContainer">
+            <thead>
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Last Name</th>
+                    <th>Country</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user, index) => {
+                    const background = index % 2 === 0 ? '#fff' : '#aaa'
+                    const printColor = color ? background : 'transparent'
+                
+                
+                return (
+
+                        <tr key={index} style={{background: printColor}}>
+                            <td>
+                                <img src={user.picture.thumbnail} alt={`${user.name.first} Photo `} />
+                            </td>
+                            <td>{user.name.first}</td>
+                            <td>{user.name.last}</td>
+                            <td>{user.location.country}</td>
+                            <td>
+                                <button>Delete</button>
+                            </td>
+
+                        </tr>
+                    )
+                })}
+
+            </tbody>
+
+        </table>
+    )
+};
+
+export { UserList };
